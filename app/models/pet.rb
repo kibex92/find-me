d5700aa0605d60da61a75dae295028f1c3874042
Boris Knezevic <boris.knezevic0@gmail.com>
2023-08-15 17:32:15 +0200

    add faker + pet model + validations + seed

---
+++ app/models/pet.rb
@@ -0,0 +1,8 @@
+class Pet < ApplicationRecord
+  SPECIES = ["cat" "mice", "dog", "snake", "monkey", "dinosaur"]
+  validates :name, presence: true
+  validates :color, presence: true
+  validates :address, presence: true
+  validates :found_on, presence: true
+  validates :species, inclusion: { in: SPECIES }
+end
